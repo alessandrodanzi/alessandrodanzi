0cb07a35f4e9e51091f184e7c777221ecd175b1c
alessandrodanzi <62661649+alessandrodanzi@users.noreply.github.com>
2020-04-08 17:49:17 +0200

    Update de_point_pattern_analysis.r

--- de_point_pattern_analysis.r
+++ de_point_pattern_analysis.r
@@ -18,3 +18,50 @@ plot(d)
 
 #and we add the density to the plot, adding more to this function without closing the plot window (or we will delete the rest)
 points(covids)
+
+
+setwd("/Users/alessandro/lab")
+load(point_pattern.RData)
+ls("point_pattern.RData")  #to check what's in there
+#covids:point pattern
+#d: density map
+
+library(spatstat)
+
+plot(d)
+
+#we can put on this map the points of the covid
+points(covids)
+
+#download coastlines .zip file from iol and copy all the files into lab folder
+
+install.packages("rgdal")
+library(rgdal)
+
+#let's input vector lines (x0y0,x1y1,x2y2,...)
+coastlines<-readOGR("ne_10m_coastline.shp")
+
+plot(d)
+points(covids)
+plot(coastlines, add=T)  #we put add because it will not erase the precedent results
+
+#change the colours and make the graph beautiful#
+#100 means that there's 100 cours from yellow to red
+cl<-colorRampPalette(c("yellow","orange","red"))(100)  
+plot(d,col=cl)
+points(covids)
+plot(coastlines, add=T)
+
+#another try
+clr<-colorRampPalette(c("light green","yellow", "orange","pink"))(100)  
+plot(d,col=clr, main="Covid19 distribution")
+points(covids)
+plot(coastlines, add=T)
+
+#export
+pdf("covid_density.pdf")
+clr<-colorRampPalette(c("light green","yellow", "orange","pink"))(100)  
+plot(d,col=clr, main="Covid19 density")
+points(covids)
+plot(coastlines, add=T)
+dev.off()
