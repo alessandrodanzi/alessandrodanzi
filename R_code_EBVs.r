a0b06f2bb1e5d53289f9fa7b6a8e5fae74da8bd5
alessandrodanzi <62661649+alessandrodanzi@users.noreply.github.com>
2020-05-27 17:28:21 +0200

    Update R_code_EBVs.r

--- R_code_EBVs.r
+++ R_code_EBVs.r
@@ -54,6 +54,8 @@ sntrpca <- rasterPCA(snt_r)
 
 summary(sntrpca$model) 
 
+
+
 clp <- colorRampPalette(c('dark grey','grey','light gray'))(100) # 
 plot(sntrpca$map,col=clp)
 
@@ -64,4 +66,79 @@ std_sntrpca <- focal(sntrpca$map$PC1, w=window, fun=sd)
 cl <- colorRampPalette(c('dark blue','green','orange','red'))(100) # 
 plot(std_sntrpca, col=cl)
 
-##############
+##############day 2:cladonia########
+setwd("/Users/alessandro/lab")
+#with the raster library we have two function to import images: raster (1 layer) and brick (several layers)
+#in this case we use the brick function because we already have in the file more than one layer
+library(raster)
+clad <- brick("cladonia_stellaris_calaita.JPG")
+#to see our image in R:
+plotRGB(clad, 1,2,3, stretch="lin")
+#window to select one window, calculate the SD and report the SD in one pixel, then the whole window shifts and report the SD in the pixel next to the other and so on
+#number 1 is an arbitriry value.
+window <- matrix(1, nrow = 3, ncol = 3)
+window
+#selected the size of the window, we do the calculation
+#we use focal function that calculate the values of several local cells
+#the clads are related (one line connect them) 
+pairs(clad)
+
+#we use the PCA
+#rename the library
+library(RStoolbox)
+#the function to do PCA is raster PCA of clad
+cladpca <- rasterPCA(clad)
+#we can use cladpca 
+summary(cladpca$model)
+
+plotRGB(cladpca$map, 1, 2, 3, stretch="lin")
+#inside cladpca there's $map with PC1, PC2, PC3
+#window(w) is the window we defined, while the function is standard deviation
+sd_clad <- focal(cladpca$map$PC1, w=window, fun=sd)
+#we aggregate to reduce the size of the layer PC1 to accelerate the calculation of the standard deviation
+PC1_agg <- aggregate(cladpca$map$PC1, fact=10)
+sd_clad_agg <- focal(PC1_agg, w=window, fun=sd)
+#we try now to make it more evident
+#par makes saveral graphs (in this case 2)
+#col to change colours, total colours 100)
+#we plot both the diversity on top of the original one, and the diversity on the aggregated pc1
+par(mfrow=c(1,2))
+cl <- colorRampPalette(c('yellow','violet','black'))(100) #
+plot(sd_clad, col=cl)
+plot(sd_clad_agg, col=cl)
+
+#in this way we can see the plot of the variation in structure and the original image
+# plot the calculation
+par(mfrow=c(1,2))
+cl <- colorRampPalette(c('yellow','violet','black'))(100) #
+plotRGB(clad, 1,2,3, stretch="lin")
+plot(sd_clad, col=cl)
+# plot(sd_clad_agg, col=cl)
+
+
+sd_sntr <- focal(snt_r10$snt_r10.4, w=window, fun=sd)
+sd_sntr <- focal(clad, w=window, fun=sd) 
+clad
+sd_sntr <- focal(clad[,1], w=window, fun=sd) 
+sd_sntr <- focal(clad$cladonia_stellaris_calaita.1, w=window, fun=sd) 
+plot(sd_sntr) 
+cl <- colorRampPalette(c('red','orange','yellow'))(100) # 
+plot(sd_sntr,col=cl) 
+cl <- colorRampPalette(c('yellow','orange','violet'))(100) # 
+plot(sd_sntr,col=cl) 
+cl <- colorRampPalette(c('yellow','violet','black'))(100) # 
+plot(sd_sntr,col=cl) 
+clado <- stack(clad, sd_sntr)
+plot(clado)
+plot(clado,col=cl)
+par(mfrow=c(1,2)) 
+plotRGB(clad,3,2,1,stretch="lin")
+plot(sd_sntr,col=cl) 
+pdf("clad_enhancement.png")
+par(mfrow=c(1,2)) 
+plotRGB(clad,3,2,1,stretch="lin")
+plot(sd_sntr,col=cl) 
+dev.off()
+q()
+
+#####################
