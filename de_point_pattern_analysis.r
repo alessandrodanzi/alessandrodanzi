1eaf49a0f54550a7eaac47f5893203732d7e339e
alessandrodanzi <62661649+alessandrodanzi@users.noreply.github.com>
2020-06-02 22:47:08 +0200

    Update de_point_pattern_analysis.r

--- de_point_pattern_analysis.r
+++ de_point_pattern_analysis.r
@@ -1,16 +1,16 @@
 #Point Pattern Analysis: Density map
 
-#install the library to develope this study
+#install the library for analysing Spatial Point Patterns to develope this study
 install.packages("spatstat")
 library(spatstat)
 
 attach(covid)
 head(covid)
 
-#covids= the name we gave to the covid spatial ppp=power point pattern, x=long(e-w), y=lat(n-s)
+#covids= the name we gave to the covid spatial ppp=power point pattern, x=long(east-weast), y=lat(north-south)
 covids<- ppp(lon, lat, c(-180,180), c(-90,90))   #c(..) is needed to take all the numbers in account together
 
-#let's associate d to the density of the covids data we just created
+#let's associate to the vector d the density of the covids data we just created
 d<- density(covids)
 
 #and create a plot of that density
@@ -20,8 +20,8 @@ plot(d)
 points(covids)
 
 
-setwd("/Users/alessandro/lab")
-load(point_pattern.RData)
+setwd("/Users/alessandro/lab") #mac
+load("point_pattern.RData")
 ls("point_pattern.RData")  #to check what's in there
 #covids:point pattern
 #d: density map
@@ -33,12 +33,13 @@ plot(d)
 #we can put on this map the points of the covid
 points(covids)
 
-#download coastlines .zip file from iol and copy all the files into lab folder
-
+#download coastlines .zip file from IOL and copy all the files into lab folder
+#rgdal package provides bindings to the 'Geospatial' Data Abstraction Library 
 install.packages("rgdal")
 library(rgdal)
 
 #let's input vector lines (x0y0,x1y1,x2y2,...)
+#read0GR within rgdal is a function that reads an OGR data source and layer into a suitable Spatial vector object
 coastlines<-readOGR("ne_10m_coastline.shp")
 
 plot(d)
@@ -46,7 +47,7 @@ points(covids)
 plot(coastlines, add=T)  #we put add because it will not erase the precedent results
 
 #change the colours and make the graph beautiful#
-#100 means that there's 100 cours from yellow to red
+#100 means that there's 100 colours from yellow to red
 cl<-colorRampPalette(c("yellow","orange","red"))(100)  
 plot(d,col=cl)
 points(covids)
@@ -58,7 +59,7 @@ plot(d,col=clr, main="Covid19 distribution")
 points(covids)
 plot(coastlines, add=T)
 
-#export
+#to export it
 pdf("covid_density.pdf")
 clr<-colorRampPalette(c("light green","yellow", "orange","pink"))(100)  
 plot(d,col=clr, main="Covid19 density")
